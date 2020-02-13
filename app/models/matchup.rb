81df5e513fbde1e5631c670c02f4377bbd3274c4
Michael Goodnow <mmgoodnow@gmail.com>
2020-02-13 16:33:32 -0500

    Add match creation callback when both lineups are set

--- app/models/matchup.rb
+++ app/models/matchup.rb
@@ -16,13 +16,57 @@ class Matchup < ApplicationRecord
     "#{away_roster.name}@#{home_roster.name}" if home_team && away_team
   end
 
-  private
-
   def create_matches
     if self.away_lineup != nil && self.home_lineup != nil
-      puts 'Valid'
-    else
-      puts 'Invalid'
+      default_match_params = {
+        status: :NOT_STARTED, home_score: 0, away_score: 0, matchup: self
+      }
+      all_match_params = []
+      all_match_params <<
+        default_match_params.merge(
+          {
+            slot: :SINGLES_1,
+            home_player_1: self.home_lineup.singles_1,
+            away_player_1: self.away_lineup.singles_1
+          }
+        )
+      all_match_params <<
+        default_match_params.merge(
+          {
+            slot: :SINGLES_2,
+            home_player_1: self.home_lineup.singles_2,
+            away_player_1: self.away_lineup.singles_2
+          }
+        )
+      all_match_params <<
+        default_match_params.merge(
+          {
+            slot: :SINGLES_3,
+            home_player_1: self.home_lineup.singles_3,
+            away_player_1: self.away_lineup.singles_3
+          }
+        )
+      all_match_params <<
+        default_match_params.merge(
+          {
+            slot: :DOUBLES_1,
+            home_player_1: self.home_lineup.doubles_1_1,
+            home_player_2: self.home_lineup.doubles_1_2,
+            away_player_1: self.away_lineup.doubles_1_1,
+            away_player_2: self.away_lineup.doubles_1_2
+          }
+        )
+      all_match_params <<
+        default_match_params.merge(
+          {
+            slot: :DOUBLES_2,
+            home_player_1: self.home_lineup.doubles_2_1,
+            home_player_2: self.home_lineup.doubles_2_2,
+            away_player_1: self.away_lineup.doubles_2_1,
+            away_player_2: self.away_lineup.doubles_2_2
+          }
+        )
+      Match.create!(all_match_params)
     end
   end
 end
